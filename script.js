256a70e7812390973ac89785419c644da40583ef
Penguin <son@vdburg.site>
2025-03-24 10:15:46 +0100

    data json completed

--- script.js
+++ script.js
@@ -20,6 +20,44 @@ function printHeroRings(data) {
         // Append the data
         ringElement.src = data[randomIndex].imageURL;
         ringElement.alt = data[randomIndex].name;
+        console.log(
+            "Ring image appended to hero section: " + data[randomIndex].name
+        );
+    }
+}
+
+/**
+ * Generate cards in the store element for 15 rings
+ * @param {*} data The ring data from the json file
+ */
+function printStoreCards(data) {
+    let storeElement = document.getElementById("storeElement");
+
+    for (let i = 0; i < 24; i++) {
+        // Store Card Element
+        let cardElement = document.createElement("div");
+        cardElement.classList.add("customStoreCard");
+
+        // Name Element
+        let nameElement = document.createElement("h2");
+        nameElement.textContent = data[i].name;
+        nameElement.classList.add("customStoreCardName");
+        cardElement.appendChild(nameElement);
+
+        // Image Element
+        let imageElement = document.createElement("img");
+        imageElement.src = data[i].imageURL;
+        imageElement.alt = data[i].name;
+        imageElement.classList.add("customHover");
+        cardElement.appendChild(imageElement);
+
+        // Effect Element
+        let effectElement = document.createElement("p");
+        effectElement.textContent = data[i].effect;
+        cardElement.appendChild(effectElement);
+        
+        storeElement.appendChild(cardElement);
+        console.log("Store card created for ring: " + data[i].name);
     }
 }
 
@@ -33,6 +71,7 @@ try {
             console.log("Ring data succesfully retrieved");
 
             printHeroRings(data);
+            printStoreCards(data);
         });
 } catch (error) {
     console.error("Error caught: ", error);
